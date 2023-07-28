dd339c3629344899e67476788845801af8c89eb5
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-07-28 09:17:39 +0300

    [done] footer and root layout

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/footer/footer";
+import Navbar from "@/components/navbar/navbar";
+
+const RootLayout = ({ children }) => {
+    return (
+        <>
+            <Navbar />
+            <main className="min-h-screen">{children}</main>
+            <Footer />
+        </>
+    );
+};
+
+export default RootLayout;
